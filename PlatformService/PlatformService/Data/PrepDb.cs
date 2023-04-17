6ece6e15fb97f1cd4ae2f010885f7d2683bdf211
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2023-04-17 15:33:06 +0200

    First Commit

---
+++ PlatformService/PlatformService/Data/PrepDb.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlatformService.Data;
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app, bool isProduction)
+    {
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProduction);
+    }
+
+    private static void SeedData(AppDbContext? context, bool isProduction)
+    {
+        if (isProduction)
+        {
+            Console.WriteLine("--> Applying Migrations...");
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not run migrations: {ex.Message}");
+            }
+        }
+
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("--> Seeding data...");
+            context.Platforms.AddRange(
+                               new Models.Platform { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                                              new Models.Platform { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                                              new Models.Platform { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                                          );
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("--> We already have data");
+        }
+    }
+}
+
---
+++ PlatformService/PlatformService/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// using in memory for out dbContext
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem")); 
+
+// register interface -> concrete implementation
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+PrepDb.PrepPopulation(app, false);
+
+app.Run();
